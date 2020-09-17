dc14d2f06464f0f315b90f41fde55cf069bb247c
Meredith McIntosh <meredith@mcintosh.io>
2020-09-17 17:57:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,7 +6,7 @@ using_concat(['1', '2', '3'], ['4', '5', '6'])
 def using_insert(array, element)
 puts array.insert
 end
-  using_insert(['1', '2', '3'], ('4')
+  using_insert(['1', '2', '3'], (3='4')
 
 # def using_uniq(array)
 #   array.uniq
