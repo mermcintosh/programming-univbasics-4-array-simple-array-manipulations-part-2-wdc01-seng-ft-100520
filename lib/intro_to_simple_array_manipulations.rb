701a31e3b07e4f69b29ca21c27f9340e00f7746f
Meredith McIntosh <meredith@mcintosh.io>
2020-09-17 17:50:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,10 +3,10 @@ def using_concat(array1, array2)
 end
 using_concat(['1', '2', '3'], ['4', '5', '6'])
   
-# def using_insert(array, element)
-# puts array.insert(element)
-# end
-#   using_insert(['1', '2', '3'], '4')
+def using_insert(array, element)
+puts array.insert(element)
+end
+  using_insert(['1', '2', '3'], '4')
 
 # def using_uniq(array)
 #   array.uniq
