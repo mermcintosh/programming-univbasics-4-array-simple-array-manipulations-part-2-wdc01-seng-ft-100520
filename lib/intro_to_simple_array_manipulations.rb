ce3827101972efce2d80d10f3ee0bf3fe865449b
Meredith McIntosh <meredith@mcintosh.io>
2020-09-17 18:58:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,7 +6,7 @@ using_concat(['1', '2', '3'], ['4', '5', '6'])
 def using_insert(array, element)
   puts array.insert(-1,element)
 end
-  using_insert(['dog', 'cat', 'rabbit', 'hamster'],('bird'))
+  using_insert(['dog', 'cat', 'rabbit', 'hamster'],(-1, 'bird'))
 
 def using_uniq(array)
   array.uniq
