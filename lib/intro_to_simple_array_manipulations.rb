0692c26b7df5a235b971e7dd7683abdc2c856cc1
Meredith McIntosh <meredith@mcintosh.io>
2020-09-17 18:45:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -24,5 +24,6 @@ using_flatten(['1', '1', '3', ['2', '3']])
 # using_delete(['dog', 'cat', 'rabbit'],'rabbit')
 
 def using_delete_at(array, integer)
-  array.delete
+  array.delete_at
 end
+using_delete_at(['dog', 'cat', 'rabbit'],2)
