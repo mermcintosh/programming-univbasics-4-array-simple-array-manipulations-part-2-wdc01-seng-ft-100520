a48c9f46a540139a3a5bce1f1224cba85887c66b
Meredith McIntosh <meredith@mcintosh.io>
2020-09-17 18:54:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,7 +4,7 @@ end
 using_concat(['1', '2', '3'], ['4', '5', '6'])
   
 def using_insert(array, element)
-  puts array.insert(-1, element)
+  puts array.insert(3, element)
 end
   using_insert(['dog', 'cat', 'rabbit'],'bird')
 
