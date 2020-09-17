dd645f9c4f9eb5932aaa1d94074316ead33cfbd4
Meredith McIntosh <meredith@mcintosh.io>
2020-09-17 17:46:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,12 +8,12 @@ using_concat(['1', '2', '3'], ['4', '5', '6'])
 # end
 #   using_insert(['1', '2', '3'], '4')
 
-def using_uniq(array)
-  array.uniq
-end 
-using_uniq (['1', '1', '2', '3', '3'])
+# def using_uniq(array)
+#   array.uniq
+# end 
+# using_uniq (['1', '1', '2', '3', '3'])
 
-def using_flatten(array)
-  array.flatten
-end
-using_flatten
+# def using_flatten(array)
+#   array.flatten
+# end
+# using_flatten
