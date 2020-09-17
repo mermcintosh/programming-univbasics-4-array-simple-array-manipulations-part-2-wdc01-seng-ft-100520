6c624f865fd9064fe77fa524e175600f35385865
Meredith McIntosh <meredith@mcintosh.io>
2020-09-17 18:33:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,10 +3,10 @@ def using_concat(array1, array2)
 end
 using_concat(['1', '2', '3'], ['4', '5', '6'])
   
-# def using_insert(array, element)
-#   puts array.insert(-1, element)
-# end
-#   using_insert(['dog', 'cat', 'rabbit'],'bird')
+def using_insert(array, element)
+  puts array.insert(-1, element)
+end
+  using_insert(['dog', 'cat', 'rabbit'],'bird')
 
 def using_uniq(array)
   array.uniq
